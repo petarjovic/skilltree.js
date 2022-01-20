a6e12d72a2a340d50df20b89f0d813df80cae8a3
Petar Jovic <petar_jovic@hotmail.com>
2022-01-20 18:29:44 -0500

    website updated

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const skillTree = new SkillTree({
-  canvas: document.getElementById("canvas"),
+  canvas: document.getElementById("skillTree"),
   title: "Sample Skill Tree",
   // free: true,
-  // respec: true,
+  respec: true,
   points: 10,
   root: new Perk({
     title: "Attack 1",
@@ -65,4 +65,75 @@ const skillTree = new SkillTree({
   }),
 });
 
+const workoutTree = new SkillTree({
+  canvas: document.getElementById("exerciseTree"),
+  title: "Exercises",
+  free: true,
+  root: new Perk({
+    title: "10 Push Ups",
+    image: "https://www.svgrepo.com/show/9896/push-up.svg",
+    children: [
+      new Perk({
+        title: "20 Push Ups",
+        image: "https://www.svgrepo.com/show/9896/push-up.svg",
+        children: [
+          new Perk({
+            title: "30 Pull Ups",
+            image: "https://www.svgrepo.com/show/9896/push-up.svg",
+            children: [
+              new Perk({
+                title: "40 Pull Ups",
+                image: "https://www.svgrepo.com/show/9896/push-up.svg",
+              }),
+            ],
+          }),
+        ],
+      }),
+      new Perk({
+        title: "10 Pull Ups",
+        image:
+          "https://www.svgrepo.com/show/47033/pulling-up-training-silhouette.svg",
+        children: [
+          new Perk({
+            title: "20 Pull Ups",
+            image:
+              "https://www.svgrepo.com/show/47033/pulling-up-training-silhouette.svg",
+            children: [
+              new Perk({
+                title: "30 Pull Ups",
+                image:
+                  "https://www.svgrepo.com/show/47033/pulling-up-training-silhouette.svg",
+                children: [
+                  new Perk({
+                    title: "40 Pull Ups",
+                    image:
+                      "https://www.svgrepo.com/show/47033/pulling-up-training-silhouette.svg",
+                  }),
+                ],
+              }),
+            ],
+          }),
+        ],
+      }),
+      new Perk({
+        title: "10 Min Run",
+        image: "https://www.svgrepo.com/show/119363/running.svg",
+        children: [
+          new Perk({
+            title: "20 Min Run",
+            image: "https://www.svgrepo.com/show/119363/running.svg",
+            children: [
+              new Perk({
+                title: "30 Min Run",
+                image: "https://www.svgrepo.com/show/119363/running.svg",
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+  }),
+});
+
 skillTree.initialize();
+workoutTree.initialize();
